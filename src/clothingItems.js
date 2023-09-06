a0f3b81be0cb6912135b7f50bb68fd5417728fb3
Malmi Hewakapuge <97165436+malmi20@users.noreply.github.com>
2023-09-06 18:56:58 +0530

    initail changes

---
+++ src/clothingItems.js
@@ -0,0 +1,47 @@
+export const clothItemsForSale = [
+    {
+      "name": "Cotton T-Shirt",
+      "type": "T-Shirt",
+      "material": "Cotton",
+      "size": "S, M, L, XL",
+      "color": "White",
+      "price": "20.99",
+      "imageUrl": "link_to_image1.jpg"
+    },
+    {
+      "name": "Denim Jeans",
+      "type": "Jeans",
+      "material": "Denim",
+      "size": "28, 30, 32, 34",
+      "color": "Blue",
+      "price": "49.99",
+      "imageUrl": "link_to_image2.jpg"
+    },
+    {
+      "name": "Summer Dress",
+      "type": "Dress",
+      "material": "Chiffon",
+      "size": "S, M, L",
+      "color": "Pink Floral",
+      "price": "34.95",
+      'imageUrl': "link_to_image3.jpg"
+    },
+    {
+      "name": "Leather Jacket",
+      "type": "Jacket",
+      "material": "Leather",
+      "size": "M, L, XL",
+      "color": "Black",
+      "price": "89.99",
+      "imageUrl": "link_to_image4.jpg"
+    },
+    {
+      "name": "Athletic Leggings",
+      "type": "Leggings",
+      "material": "Spandex",
+      "size": "XS, S, M",
+      "color": "Black",
+      "price": "28.50",
+      "imageUrl": "link_to_image5.jpg"
+    }
+  ];
